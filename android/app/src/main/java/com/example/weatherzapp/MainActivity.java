87ebdf91837a8e9395aaf548038d6c35fe7b2fca
chelsamca <chelsamca@gmail.com>
2024-01-03 22:43:08 -0800

    weatherapp

---
+++ android/app/src/main/java/com/example/weatherzapp/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.weatherzapp;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/weatherz/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  //api key
+
+  //fetch weather
+
+  //weather animations
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
